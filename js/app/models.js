ddf06a9cc7034b438e57ef20f4405f1e14a85137
Pedro Goncalves Rossi Rodrigues <pgrr@ug1c30.windows.cin.ufpe.br>
2016-12-16 11:30:59 -0300

    Camera model ready and methods working and begining of ilumination methods programing

--- js/app/models.js
+++ js/app/models.js
@@ -2,48 +2,48 @@ function Vetor(x, y, z) {
   this.x = x;
   this.y = y;
   this.z = z;
-  Vetor.prototype.normalizar = function() {
-    var norma = Math.sqrt(this.x*this.x + this.y*this.y + this.z*this.z);
-    this.x/=norma;
-    this.y/=norma;
-    this.z/=norma;
-  };
-  Vetor.prototype.produtoEscalar = function(v) {
-    return (this.x*v.x + this.y*v.y + this.z*v.z);
-  };
-  Vetor.prototype.produtoVetorial = function(v) {
-    var x = this.y*v.z - this.z*v.y;
-    var y = this.z*v.x - this.x*v.z;
-    var z = this.x*v.y - this.y*v.x;
-    return new Vetor(x, y, z);
-  };
-  Vetor.prototype.multiplicar = function(k) {
-    this.x*=k;
-    this.y*=k;
-    this.z*=k;
-  };
-  Vetor.prototype.projecaoOrtogonal = function(v) {
-    var k = (this.produtoEscalar(v))/(v.produtoEscalar(v));
-    var r = new Vetor(this.x, this.y, this.z);
-    r.multiplicar(k);
-    return r;
-  };
-  Vetor.prototype.sub = function(v) {
-    return new Vetor(this.x-v.x, this.y-v.y, this.z-v.z);
-  };
-  Vetor.prototype.gramSchmidt = function(v) {
-    return this.sub(v.projecaoOrtogonal(this));
-  };
 }
+Vetor.prototype.normalizar = function() {
+  var norma = Math.sqrt(this.x*this.x + this.y*this.y + this.z*this.z);
+  this.x/=norma;
+  this.y/=norma;
+  this.z/=norma;
+};
+Vetor.prototype.produtoEscalar = function(v) {
+  return (this.x*v.x + this.y*v.y + this.z*v.z);
+};
+Vetor.prototype.produtoVetorial = function(v) {
+  var x = this.y*v.z - this.z*v.y;
+  var y = this.z*v.x - this.x*v.z;
+  var z = this.x*v.y - this.y*v.x;
+  return new Vetor(x, y, z);
+};
+Vetor.prototype.multiplicar = function(k) {
+  this.x*=k;
+  this.y*=k;
+  this.z*=k;
+};
+Vetor.prototype.projecaoOrtogonal = function(v) {
+  var k = (v.produtoEscalar(this))/(this.produtoEscalar(this));
+  var r = new Vetor(this.x, this.y, this.z);
+  r.multiplicar(k);
+  return r;
+};
+Vetor.prototype.sub = function(v) {
+  return new Vetor(this.x-v.x, this.y-v.y, this.z-v.z);
+};
+Vetor.prototype.gramSchmidt = function(v) {
+  return this.sub(v.projecaoOrtogonal(this));
+};
 
 function Ponto(x, y, z) {
   this.x = x;
   this.y = y;
   this.z = z;
-  Ponto.prototype.add = function(p) {
-    return new Point(this.x + p.x, this.y + p.y, this.z + p.z)
-  };
 }
+Ponto.prototype.add = function(p) {
+  return new Point(this.x + p.x, this.y + p.y, this.z + p.z)
+};
 
 function Triangulo(p1, p2, p3) {
   this.p1 = p1;
@@ -59,16 +59,16 @@ function Camera(c, n, v, d, hx, hy) {
   this.hx = hx;
   this.hy = hy;
   this.alfa = [];
-  Camera.prototype.genAlfa = function() {
-    this.n.normalizar();
-    this.v.gramSchmidt(this.n);
-    this.v.normalizar();
-    var u = this.n.produtoVetorial(v);
-    this.alfa.push(u);
-    this.alfa.push(this.v);
-    this.alfa.push(this.n);
-  }
 }
+Camera.prototype.genAlfa = function() {
+  this.n.normalizar();
+  this.v = this.v.gramSchmidt(this.n);
+  this.v.normalizar();
+  var u = this.n.produtoVetorial(this.v);
+  this.alfa.push([u.x, u.y, u.z]);
+  this.alfa.push([this.v.x, this.v.y, this.v.z]);
+  this.alfa.push([this.n.x, this.n.y, this.n.z]);
+};
 
 function Cor(r, g, b) {
   this.r = r;
@@ -90,3 +90,6 @@ function Iluminacao(pl, ka, ia, kd, od, ks, il, n) {
   this.il = il;
   this.n = n;
 }
+Iluminacao.prototype.getPlVista = function(alfa) {
+  this.pl.x = this.pl.x;
+};
